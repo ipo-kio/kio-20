7b35696551adfea404236a331408e19063fb76f7
ilya <iposov@gmail.com>
2019-01-11 01:48:37 +0300

    initial commit

---
+++ tasks/task.js
@@ -0,0 +1,52 @@
+import 'task.scss'; //TODO заменить имя файла со стилями
+
+export class Task { //TODO название класса должно совпадать с id задачи, но с заглавной буквы
+
+    /**
+     *
+     * @param settings Объект с настройками задачи. В данный момент, внутри объекта settings ожидается только поле level,
+     * которое может быть 0, 1 или 2.
+     */
+    constructor(settings) {
+        this.settings = settings;
+
+        //TODO здесь можно совершить инициализацию, которая не зависит от положения в DOM и kioapi
+    }
+
+    /**
+     * Идентификатор задачи, используется сейчас только как ключ для
+     * хранения данных в localstorage
+     * @returns {string} идентификатор задачи
+     */
+    id() {
+        return 'task-id' + this.settings.level; //TODO заменить task-id на реальный id задачи
+    }
+
+    /**
+     *
+     * @param domNode
+     * @param kioapi
+     * @param preferred_width
+     */
+    initialize(domNode, kioapi, preferred_width) {
+        this.kioapi = kioapi;
+
+        //TODO реализовать инициализацию
+    }
+
+    parameters() {
+        return [
+            //TODO добавить список параметров
+        ];
+    }
+
+    solution() {
+        //TODO вернуть объект с описанием решения участника
+    }
+
+    loadSolution(solution) {
+        // Все объекты, которые сюда передаются, были ранее возвращены методом solution,
+        // но проверять их все равно необходимо.
+        //TODO загрузить объект с решением участника.
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,142 @@
+'use strict';
+
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const merge = require('deepmerge');
+const path = require('path');
+const fs = require('fs');
+
+const sourceFolders = [
+    path.join(__dirname, 'tasks')
+];
+
+//envirnoment env.mode = 'prod'|'dev'
+
+module.exports = function (env) {
+    let config = {
+        entry: {
+            //added for all tasks in tasks folder
+            //'taskname': 'taskname/taskname.js',
+        },
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: '[name].js',
+            library: '[name]'
+        },
+        resolve: {
+            modules: sourceFolders
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader', //Why loader instead of use?
+                    include: sourceFolders,
+                    options: {
+                        presets: [
+                            ['env', {"modules": false}], //this is 'env' preset with options
+                        ],
+                        plugins: [
+                            "transform-object-rest-spread",
+                            "transform-class-properties"
+                            // ["transform-object-rest-spread", { "useBuiltIns": true }]
+                        ]
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    include: sourceFolders,
+                    use: ExtractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: [{
+                            loader: "css-loader",
+                            options: {
+                                url: false
+                            }
+                        }, "sass-loader"]
+                    })
+                }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin("[name].css"),
+            // added for all tasks in tasks folder:
+            // new CopyWebpackPlugin([
+            //     {from: './tasks/taskname/img/*', to: './taskname-resources', flatten: true},
+            // ])
+        ]
+    };
+
+    let debugConfig = {
+        devtool: 'source-map',
+        output: {
+            pathinfo: true
+        }
+    };
+
+    let productionConfig = {
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                comments: false
+            })
+        ]
+    };
+
+    let arrayMerge = function (destArray, sourceArray, options) {
+        return destArray.concat(sourceArray);
+    };
+
+    find_all_tasks_and_add_to_config(config);
+
+    if (env && env.mode === 'prod') {
+        return merge(config, productionConfig, {arrayMerge: arrayMerge});
+    } else
+        return merge(config, debugConfig, {arrayMerge: arrayMerge});
+};
+
+function find_all_tasks_and_add_to_config(config) {
+    config.entry = {};
+
+    let task_html_template = fs.readFileSync('./tasks/task.html', {encoding: "utf8"});
+
+    if (!fs.existsSync('./dist'))
+        fs.mkdirSync('./dist');
+
+    fs.readdirSync('./tasks').forEach(file => {
+        add_task_to_config(file, config, task_html_template);
+    });
+}
+
+function process_html_template(task_html_template, task_name) {
+    let capitalized_task_name = task_name.charAt(0).toUpperCase() + task_name.slice(1);
+
+    function replace_all(where, what, to) {
+        let replacement = new RegExp('\\[' + what + '\\]', 'g');
+        return where.replace(replacement, to);
+    }
+
+    task_html_template = replace_all(task_html_template, 'TASKNAME', task_name);
+    task_html_template = replace_all(task_html_template, 'TASKNAME\\|CAPITALIZE', capitalized_task_name);
+
+    return task_html_template;
+}
+
+function add_task_to_config(task_name, config, task_html_template) {
+    if (task_name.indexOf('.') >= 0) // skip non directories
+        return;
+
+    //add entries
+    config.entry[task_name] = task_name + '/' + task_name + '.js';
+
+    //copy html
+    let output_html = process_html_template(task_html_template, task_name);
+    fs.writeFileSync('./dist/' + task_name + '.html', output_html, {encoding: "utf8"});
+
+    //copy assets
+    config.plugins.push(
+        new CopyWebpackPlugin([
+            {from: './tasks/' + task_name + '/res/*', to: './' + task_name + '-resources', flatten: true},
+        ])
+    );
+}
