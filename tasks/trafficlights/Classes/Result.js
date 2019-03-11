e4c767e4cc7d268e2dbfa3f0eca49fa1224c3463
peter <pe68@marathonbet.ru>
2019-03-11 18:04:50 +0300

    Stable version without images

--- tasks/trafficlights/Classes/Result.js
+++ tasks/trafficlights/Classes/Result.js
@@ -1,7 +1,6 @@
-export class Result
-{
-	_tikCount = 0;
-	_waitCount = 0;
-	_stopCount = 0;
-	_pointCount = 0;
-} 
+export class Result {
+  _tikCount = 0
+  _waitCount = 0
+  _stopCount = 0
+  _pointCount = 0
+}
